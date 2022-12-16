4beb622b643a503534af0f4cc39eff4fc184b2df
Your Name <you@example.com>
2022-12-16 14:38:15 +0300

    1

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -7,5 +7,4 @@ const store = configureStore({
   middleware: [thunk],
   devTools: process.env.NODE_ENV !== 'production',
 })
-setInterval(() => console.log(store.getState()), 5000)
 export default store
